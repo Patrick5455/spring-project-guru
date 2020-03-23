22241271cb64e3d4ee84552ba9275baa674e5171
Patrick5455 <patrickojunde@gmail.com>
2020-03-23 11:14:43 +0100

    using @Primary annotation for in java config

--- diexample/src/main/java/guru/springframework/javaconfig/HelloConfig.java
+++ diexample/src/main/java/guru/springframework/javaconfig/HelloConfig.java
@@ -15,6 +15,7 @@ import guru.springframework.services.HelloWorldServiceEnglishImpl;
 import guru.springframework.services.HelloWorldSpanishImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
@@ -50,33 +51,37 @@ public class HelloConfig {
     // it collects an HelloWorldFactory class's reference as parameter
     // to call an HelloWorldFactory method - createHelloWorldService
     @Profile("english")
+    @Primary
     public HelloWorldService helloWorldServiceEnglish (HelloWorldFactory factory){
         return factory.createHelloWorldService("en");
     }
 
     @Bean
     @Profile("spanish")
+    @Primary
+    // the @Primary annotation is used to tell spring to run prioritise a particular bean
+    // however, the bean profile must also be indicated in the application properties
     public  HelloWorldService helloWorldServiceSpanish (HelloWorldFactory factory){
         return factory.createHelloWorldService("es");
     }
 
-//    @Bean
-//    public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory){
-//        return factory.createHelloWorldService("fr");
-//    }
-//
-//    @Bean
-//    public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory){
-//        return factory.createHelloWorldService("de");
-//    }
-//
-//    @Bean
-//    public HelloWorldService helloWorldServicePolish (HelloWorldFactory factory){
-//        return factory.createHelloWorldService("pl");
-//    }
-//
-//    @Bean
-//    public HelloWorldService helloWorldServiceRussian (HelloWorldFactory factory){
-//         return factory.createHelloWorldService("ru");
-//    }
+    @Bean
+    public HelloWorldService helloWorldServiceFrench(HelloWorldFactory factory){
+        return factory.createHelloWorldService("fr");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServiceGerman(HelloWorldFactory factory){
+        return factory.createHelloWorldService("de");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServicePolish (HelloWorldFactory factory){
+        return factory.createHelloWorldService("pl");
+    }
+
+    @Bean
+    public HelloWorldService helloWorldServiceRussian (HelloWorldFactory factory){
+         return factory.createHelloWorldService("ru");
+    }
 }
